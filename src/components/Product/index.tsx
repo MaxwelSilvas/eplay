bfa516a9d9a0bcd14afc4e390bad71d04e5c5324
Maxwel Silva <maxwel101@outlook.com.br>
2025-02-08 19:35:26 -0300

    componetizando part 3

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,18 +1,34 @@
 import Tag from '../Tag'
-import { Card, Descricao, Titulo } from './styles'
+import { Card, Descricao, Titulo, Infos } from './styles'
 
-const Product = () => (
+type Props = {
+  title: string
+  category: string
+  system: string
+  description: string
+  infos: string[]
+  image: string
+}
+
+const Product = ({
+  title,
+  category,
+  system,
+  description,
+  infos,
+  image
+}: Props) => (
   <Card>
-    <img src="//placehold.co/222x250" alt="Imagem de exemplo" />
-    <Titulo>Nome do Jogo</Titulo>
-    <Tag>Categoria</Tag>
-    <Tag>Windows</Tag>
-    <Descricao>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae
-      saepe placeat aperiam consequatur, similique ut facere quasi iste eligendi
-      ea exercitationem expedita eveniet? Impedit accusamus quis excepturi
-      repellendus, et iste?
-    </Descricao>
+    <img src={image} alt={title} />
+    <Infos>
+      {infos.map((info) => (
+        <Tag key={info}>{info}</Tag>
+      ))}
+    </Infos>
+    <Titulo>{title}</Titulo>
+    <Tag>{category}</Tag>
+    <Tag>{system}</Tag>
+    <Descricao>{description}</Descricao>
   </Card>
 )
 
--- src/components/ProductsList/index.tsx
+++ src/components/ProductsList/index.tsx
@@ -1,20 +1,48 @@
 import Product from '../Product'
 import { Container, List } from './styles'
 
-type Props = {
+export type Props = {
   title: string
   background: 'gray' | 'black'
 }
 
 const ProductList = ({ background, title }: Props) => (
-  <Container>
+  <Container background={background}>
     <div className="container">
       <h2>{title}</h2>
       <List>
-        <Product />
-        <Product />
-        <Product />
-        <Product />
+        <Product
+          category="Ação"
+          description="teste"
+          image="https://dummyimage.com/222x250/000/fff"
+          infos={['-10%', 'R$ 150,00']}
+          system="Windows"
+          title="Nome do jogo"
+        />
+        <Product
+          category="Ação"
+          description="teste"
+          image="https://dummyimage.com/222x250/000/fff"
+          infos={['-10%', 'R$ 150,00']}
+          system="Windows"
+          title="Nome do jogo"
+        />
+        <Product
+          category="Ação"
+          description="teste"
+          image="https://dummyimage.com/222x250/000/fff"
+          infos={['-10%', 'R$ 150,00']}
+          system="Windows"
+          title="Nome do jogo"
+        />
+        <Product
+          category="Ação"
+          description="teste"
+          image="https://dummyimage.com/222x250/000/fff"
+          infos={['-10%', 'R$ 150,00']}
+          system="Windows"
+          title="Nome do jogo"
+        />
       </List>
     </div>
   </Container>
