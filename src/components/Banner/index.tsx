d4ca8d6b9a4a03a716ff787db6b2728a2c336c29
Maxwel Silva <maxwel101@outlook.com.br>
2025-06-09 22:01:05 -0300

    configurando o redux

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -2,18 +2,11 @@ import { Imagem, Precos, Titulo } from './styles'
 
 import Tag from '../Tag'
 import Button from '../Button'
-import { useEffect, useState } from 'react'
-import { Game } from '../../pages/Home/Index'
 import { formataPreco } from '../ProductsList'
+import { useGetFeatureGameQuery } from '../../services/api'
 
 const Banner = () => {
-  const [game, setGame] = useState<Game>()
-
-  useEffect(() => {
-    fetch('https://fake-api-tau.vercel.app/api/eplay/destaque')
-      .then((res) => res.json())
-      .then((res) => setGame(res))
-  }, [])
+  const { data: game, isLoading } = useGetFeatureGameQuery()
 
   if (!game) {
     return <h3>Carregando...</h3>
